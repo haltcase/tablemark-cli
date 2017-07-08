f24f7b68534f1c1a73e0d4a88e13902ea304db21
citycide <thecitycide@gmail.com>
2017-07-07 23:46:00 -0500

    test: fix expected output

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ import fn from './'
 const expected = [
   'name | repo | desc',
   '----- | ----- | -----',
-  'Trilogy | [citycide/trilogy](https://github.com/citycide/trilogy) | No-hassle SQLite with type-casting schema models and support for native & pure JS backends.',
+  'trilogy | [citycide/trilogy](https://github.com/citycide/trilogy) | No-hassle SQLite with type-casting schema models and support for native & pure JS backends.',
   'strat | [citycide/strat](https://github.com/citycide/strat) | Functional-ish JavaScript string formatting, with inspirations from Python.',
   'tablemark-cli | [citycide/tablemark-cli](https://github.com/citycide/tablemark-cli) | Generate markdown tables from JSON data at the command line.'
 ].join(EOL) + EOL
